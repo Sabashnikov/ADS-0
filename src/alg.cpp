728768dd29ebc2df5eb3e1217efce2095c6789e0
Sabashnikov <160218448+Sabashnikov@users.noreply.github.com>
2024-02-21 19:14:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,18 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    if (a <= b)
+{
+    for (int i = a; i >= 1; i--)
+    {
+        if (a % i == 0 && b % i == 0)
+            return i;
+    }
+}
+else
+    for (int i = b; i >= 1; i--)
+    {
+        if (a % i == 0 && b % i == 0)
+            return i;
+    }
 }
